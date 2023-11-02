5e34b5ee666a6e0d6eb4f7d1cbf5644737d4a201
DAVINA SINGH <davina_singh@outlook.com>
2023-11-02 15:46:44 +0000

    final

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
 const express = require("express");
 const cors = require("cors");
-const { response } = require("express");
 require("dotenv").config();
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 8080;
-const axios = require
+const axios = require("axios");
 
 app.get("/", (request, response) => {
-    response.json("You are looking at my route... how roude!");
+  response.json("You are looking at my root route... how roude! (unsplosh)");
 });
 
+// http://localhost:8080/photos?search=sunset
+// request.query.search === sunset
 app.get("/photos", async (request, response) => {
-    const API = `https://api.unsplash.com/search/photos?client_id=${process.env.PORT}&query=wizard`;
-    const res = await axios.get(API)
-    const wrangledData = res.data.results.map(())=>{
-        return {
-            image_url: image.
-            photographer:
-        }
+  const search = request.query.search;
+  const API = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}&query=${search}`;
 
-    })
-    response.json("Photos endpoint");
+  const res = await axios.get(API);
+
+  const wrangledData = res.data.results.map((image) => {
+    return {
+      img_url: image.urls.regular,
+      photographer: image.user.name,
+    };
+  });
+
+  response.json(wrangledData);
 });
 
-app.listen(POORT, () => console.log('App is running on port ${PORT}'));
-
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
